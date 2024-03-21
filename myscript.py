644d8dffc017fa496a2952aa18efb32635c974f3
yaourtman <yaourtman852@gmail.com>
2024-03-21 19:02:30 -0400

    up

--- myscript.py
+++ myscript.py
@@ -3,10 +3,10 @@ Automatisation de git bisect pour trouver le premier
 commit pour lequel les tests échouent.
 """
 
-import subprocess
+import os
 
 good_hash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 bad_hash = "c1a4be04b972b6c17db242fc37752ad517c29402"
 
-subprocess.call(f"git bisect start {bad_hash} {good_hash}")
-subprocess.call("git bisect run python manage.py test")
+os.system(f"git bisect start {bad_hash} {good_hash}")
+os.system("git bisect run python manage.py test")
