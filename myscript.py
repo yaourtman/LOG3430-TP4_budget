0ba6f471c1b875cd13b2dce211a770b0b244669b
yaourtman <yaourtman852@gmail.com>
2024-03-21 18:52:47 -0400

    change os to subprocess

--- myscript.py
+++ myscript.py
@@ -3,11 +3,7 @@ Automatisation de git bisect pour trouver le premier
 commit pour lequel les tests échouent.
 """
 
-import os
+import subprocess
 
-if __name__ == "__main__":
-    try:
-        os.system("git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
-        os.system("git bisect run python manage.py test")
-    except:
-        pass
+subprocess.call("git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
+subprocess.call("git bisect run python manage.py test")
