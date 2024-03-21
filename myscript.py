da72ff49d1d475b4d5acccf67bbfe677f883a6a2
yaourtman <yaourtman852@gmail.com>
2024-03-21 15:14:15 -0400

    add script

---
+++ myscript.py
@@ -0,0 +1,10 @@
+"""
+Automatisation de git bisect pour trouver le premier
+commit pour lequel les tests échouent.
+"""
+
+import os
+
+if __name__ == "__main__":
+    os.system("git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
+    os.system("git bisect run python manage.py test")
