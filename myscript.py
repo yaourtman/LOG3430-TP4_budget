b71191eda1dcd0e2b5d05492d594adc4c9cdcf37
yaourtman <yaourtman852@gmail.com>
2024-03-21 18:56:54 -0400

    update script

--- myscript.py
+++ myscript.py
@@ -5,5 +5,8 @@ commit pour lequel les tests échouent.
 
 import subprocess
 
-subprocess.call("git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
+good_hash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+bad_hash = "c1a4be04b972b6c17db242fc37752ad517c29402"
+
+subprocess.call(f"git bisect start {bad_hash} {good_hash}")
 subprocess.call("git bisect run python manage.py test")
